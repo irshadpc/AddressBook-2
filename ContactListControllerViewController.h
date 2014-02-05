cc6e48393637feb41d4d2c0ab7317cebe385c72d
majorgilles <major.gilles@gmail.com>
2014-02-05 15:50:13 +0100

    Created the first part of the interface in the story board (navigation controller embedding a table view controller). Created custom prototype cell and datasource methods of the table view.

--- ContactListControllerViewController.h
+++ ContactListControllerViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ContactListControllerViewController.h
 //  AddressBook
 //
 //  Created by Gilles Major on 05/02/14.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ContactListControllerViewController : UITableViewController
 
 @end
